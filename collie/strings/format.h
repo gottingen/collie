bf583537911a7a5c2b43cff773b1ea885260b131
jeff.li <lijippy@163.com>
2024-04-14 02:40:18 +0800

    add format function

--- collie/strings/format.h
+++ collie/strings/format.h
@@ -15,6 +15,7 @@
 
 #ifndef COLLIE_STRINGS_FORMAT_H_
 #define COLLIE_STRINGS_FORMAT_H_
+
 #include <collie/strings/fmt/format.h>
 #include <collie/strings/fmt/ostream.h>
 #include <collie/strings/fmt/printf.h>
@@ -26,7 +27,135 @@
 #include <collie/strings/fmt/args.h>
 
 namespace collie {
+
+    template<typename String = std::string, typename T>
+    [[nodiscard]] inline String to_str(const T &t) {
+        String result;
+        fmt::memory_buffer buf;
+        fmt::format_to(std::back_inserter(buf), "{}", t);
+        return String(buf.data(), buf.size());
+    }
+
     using namespace fmt;
+
+    template<typename String = std::string, typename ...Args>
+    void format_append(String *dst, std::string_view fmt, Args &&... args) {
+        fmt::memory_buffer buf;
+        fmt::format_to(std::back_inserter(buf), fmt, std::forward<Args>(args)...);
+        dst->append(buf.data(), buf.size());
+    }
+
+    template<typename String = std::string, typename T>
+    void format_append(String *dst, const T &t) {
+        fmt::memory_buffer buf;
+        fmt::format_to(std::back_inserter(buf), "{}", t);
+        dst->append(buf.data(), buf.size());
+    }
+
+    template<typename String = std::string, typename ...Args>
+    String format_range(std::string_view fmt, const std::tuple<Args...> &tuple, std::string_view sep) {
+        fmt::memory_buffer view_buf;
+        fmt::format_to(std::back_inserter(view_buf), fmt, fmt::join(tuple, sep));
+        return String(view_buf.data(), view_buf.size());
+    }
+
+    template<typename String = std::string, typename T>
+
+    String format_range(std::string_view fmt, std::initializer_list<T> list, std::string_view sep) {
+        fmt::memory_buffer view_buf;
+        fmt::format_to(std::back_inserter(view_buf), fmt, fmt::join(list, sep));
+        return String(view_buf.data(), view_buf.size());
+    }
+
+    template<typename It, typename Sentinel, typename String = std::string>
+    String format_range(std::string_view fmt, It begin, Sentinel end, std::string_view sep) {
+        fmt::memory_buffer view_buf;
+        fmt::format_to(std::back_inserter(view_buf), fmt,
+                         fmt::join(std::forward<It>(begin), std::forward<Sentinel>(end), sep));
+        return String(view_buf.data(), view_buf.size());
+    }
+
+    template<typename String = std::string, typename Range>
+    String format_range(std::string_view fmt, Range &&range, std::string_view sep) {
+        fmt::memory_buffer view_buf;
+        fmt::format_to(std::back_inserter(view_buf), fmt, fmt::join(std::forward<Range>(range), sep));
+        return String(view_buf.data(), view_buf.size());
+    }
+
+    /// format_range_append
+    template<typename String = std::string, typename ...Args>
+    void
+    format_range_append(String *dst, std::string_view fmt, const std::tuple<Args...> &tuple, std::string_view sep) {
+        fmt::memory_buffer view_buf;
+        fmt::format_to(std::back_inserter(view_buf), fmt, fmt::join(tuple, sep));
+        dst->append(view_buf.data(), view_buf.size());
+    }
+
+    template<typename String = std::string, typename T>
+
+    void format_range_append(String *dst, std::string_view fmt, std::initializer_list<T> list, std::string_view sep) {
+        fmt::memory_buffer view_buf;
+        fmt::format_to(std::back_inserter(view_buf), fmt, fmt::join(list, sep));
+        dst->append(view_buf.data(), view_buf.size());
+    }
+
+    template<typename String = std::string, typename It, typename Sentinel>
+    void format_range_append(String *dst, std::string_view fmt, It begin, Sentinel end, std::string_view sep) {
+        fmt::memory_buffer view_buf;
+        fmt::format_to(std::back_inserter(view_buf), fmt, fmt::join(begin, end, sep));
+        dst->append(view_buf.data(), view_buf.size());
+    }
+
+    template<typename String = std::string, typename Range>
+    void format_range_append(String *dst, std::string_view fmt, Range &&range, std::string_view sep) {
+        fmt::memory_buffer view_buf;
+        fmt::format_to(std::back_inserter(view_buf), fmt, fmt::join(std::forward<Range>(range), sep));
+        dst->append(view_buf.data(), view_buf.size());
+    }
+
+
+    template<typename... T>
+    inline void println(std::FILE *f, format_string<T...> fmt, T &&... args) {
+        return fmt::print(f, "{}\n", fmt::format(fmt, std::forward<T>(args)...));
+    }
+
+    template<typename... T>
+    inline void println(format_string<T...> fmt, T &&... args) {
+        return println(stdout, fmt, std::forward<T>(args)...);
+    }
+
+    template<typename... T>
+    void println(std::ostream &os, format_string<T...> fmt, T &&... args) {
+        fmt::print(os, "{}\n", fmt::format(fmt, std::forward<T>(args)...));
+    }
+
+    template<typename... Args>
+    void println(std::wostream &os,
+                 basic_format_string<wchar_t, type_identity_t<Args>...> fmt,
+                 Args &&... args) {
+        print(os, L"{}\n", fmt::format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename ...Args>
+    void println(const text_style &ts, std::string_view fmt, Args &&... args) {
+        fmt::print(stdout, ts, "{}\n", format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename ...Args>
+    void println(const color &c, std::string_view fmt, Args &&... args) {
+        fmt::print(stdout, fg(c), "{}\n", format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename ...Args>
+    void println(std::string_view fmt, Args &&... args) {
+        fmt::print(stdout, "{}\n", format(fmt, std::forward<Args>(args)...));
+    }
+
+    template<typename ...Args>
+    void fprintln(std::FILE *file, std::string_view fmt, Args &&... args) {
+        fmt::print(file, "{}\n", format(fmt, std::forward<Args>(args)...));
+    }
+
 }
 
 #endif  // COLLIE_STRINGS_FORMAT_H_
